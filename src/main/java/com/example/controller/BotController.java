ddfe956b1aa8a4b58ce9f8bf3dba8fdd26d9efb6
awatef <awatef@localhost.localdomain>
2018-04-04 13:10:51 +0200

    app_finished

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
