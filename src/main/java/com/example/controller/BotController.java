b88cd23031a9c2d581aa574ab9e277bb85d0ce6c
awatef <awatef@localhost.localdomain>
2018-04-11 12:26:11 +0200

    carousel

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -419,8 +419,8 @@ public class BotController {
 								"FROM: " + userInformationRepository.findOne(requests.get(i).getFromUser())
 										.getUserName() + "\nDETAIL: " + requests.get(i).getDetail(),
 								Arrays.asList(
-										new PostbackAction("Approve", "request A", "Approve Request"),
-										new PostbackAction("Disapprove", "request B", "Disapprove Request")));
+										new PostbackAction("Approve", "request " + requests.get(i).getRequestId(), "Approve Request"),
+										new PostbackAction("Disapprove", "request " + requests.get(i).getRequestId(), "Disapprove Request")));
 						
 						carouselColumnList.add(carouselColumn);
 						logger.info("carousel liiiiiiiiiiiiiiist*************************");
