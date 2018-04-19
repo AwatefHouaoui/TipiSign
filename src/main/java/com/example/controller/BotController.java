ca8856d99ec82b52337cc4b14357bc7c1814d1d0
awatef <awatef@localhost.localdomain>
2018-04-19 15:48:46 +0200

    internationalisation

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -537,8 +537,7 @@ public class BotController {
 
 							typeCQuestion(
 									"Are you sure you want to approve the request of "
-											+ userInformationRepository.findOne(r.getFromUser()).getUserName()
-											+ "\nTitled: " + r.getTitle(),
+											+ userInformationRepository.findOne(r.getFromUser()).getUserName(),
 									"Yes Approve", "Yes Approve " + r.getRequestId(), "No Cancel", "No Cancel"  + r.getRequestId(), "Confirm",
 									TOKEN, userId);
 
@@ -564,8 +563,7 @@ public class BotController {
 
 							typeCQuestion(
 									"Are you sure you want to disapprove the request of "
-											+ userInformationRepository.findOne(r.getFromUser()).getUserName()
-											+ "\nTitled: " + r.getTitle(),
+											+ userInformationRepository.findOne(r.getFromUser()).getUserName(),
 									"Yes Disapprove", "Yes Disapprove " + r.getRequestId(), "No Cancel ", "No Cancel "  + r.getRequestId(),
 									"Confirm", TOKEN, userId);
 
