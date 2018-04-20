11b36135fb654ac9ada36aec9c83be5cda769023
awatef <awatef@localhost.localdomain>
2018-04-20 19:59:29 +0200

    see.more.function

--- src/main/java/com/example/dao/AuthorityRepository.java
+++ src/main/java/com/example/dao/AuthorityRepository.java
@@ -3,9 +3,11 @@ package com.example.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.example.entities.Authority;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+	@Query("select a from Authority ")
 	public Page<Authority> findAllAuthority(Pageable pageable);
 
 }
